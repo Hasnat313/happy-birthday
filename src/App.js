a23b466bf8d1d96789269ec6c6836178a062c2b0
Muhammad Hasnat Saghir <hasnatsaghir@gmail.com>
2023-03-13 13:06:12 +0500

    birthday code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import image from "../src/ali.png";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const styles = {
+		backgroundColor: "#f0e68c",
+		textAlign: "center",
+		padding: "30px",
+		fontFamily: "Arial, sans-serif",
+		color: "#8b0000",
+		fontSize: "24px",
+	};
+	return (
+		<div style={{ backgroundColor: "#f0e68c", textAlign: "center", padding: "50px" }}>
+			<h1 style={{ color: "#8b0000", fontSize: "50px", fontFamily: "Arial, sans-serif" }}>
+				Happy Birthday Ali Hassan!🍪🍰🍦
+			</h1>
+			<img
+				src={image}
+				alt="Birthday cake"
+				style={{ width: "250px", animation: "spin 2s linear infinite" }}
+			/>
+			<p style={styles}>
+				You're such a generous, kind, fabulous person and I'm so lucky to have you as a friend.
+				Wishing you an extra special birthday and best wishes for the year ahead. 🍰 | 🍰🍰🍰 |
+				🥛🍪🍰🍦
+			</p>
+		</div>
+	);
 }
 
 export default App;
